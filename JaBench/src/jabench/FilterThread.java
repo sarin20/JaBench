99398f6f6fac1089c0d28909de2ddacdff9d16b0
sarin <sarin2.0@gmail.com>
2017-10-07 15:01:59 +0300

    thread tm

--- JaBench/src/jabench/FilterThread.java
+++ JaBench/src/jabench/FilterThread.java
@@ -15,14 +15,18 @@ public class FilterThread extends Thread {
     private int il;
     private int start;
     private int tstep;
+    
+    private long et = -1;
+    private int dc;
 
     public FilterThread(Integer[] result, int il, int start, int tstep) {
+        super("len: " + result.length + "il: " + il + " start: " + start + " tstep: " + tstep);
         reInit(result, il, start, tstep);
     }
 
     public final void reInit(Integer[] r, int il, int start, int tstep) {
         this.result = r;
-        this.il = il;
+        this.il = il; 
         this.start = start;
         this.tstep = tstep;
     }
@@ -31,17 +35,33 @@ public class FilterThread extends Thread {
 
     @Override
     public void run() {
+        long st = System.nanoTime();
+        
         for (int i = il + start; i < result.length; i += tstep) {
             for (int k = 0; k < il; k++) {
                 final int rk = result[k];
                 final int ri = result[i];
                 final int dv = ri / rk;
+//                System.out.println(ri + " / " + rk);
                 final int mlp = dv * rk;
+                dc++;
                 if (rk != 1 && mlp == ri) {
                     result[i] = 1;
+                    break;
                 }
             }
         }
+        et = System.nanoTime()- st;
     }
+
+    public long getEt() {
+        return et;
+    }
+
+    public int getDc() {
+        return dc;
+    }
+    
+    
 
 }
